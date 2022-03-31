5af8d6d6edc84b0c9567bc0e89035e29d9b4b7b4
heeheesun <heeheesun00@gmail.com>
2022-03-31 17:31:36 +0900

    @Controller -> @RestController

--- src/main/java/com/hello/controller/HelloController.java
+++ src/main/java/com/hello/controller/HelloController.java
@@ -3,29 +3,26 @@ package com.hello.controller;
 import com.hello.dto.BookDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class HelloController {
 
     @GetMapping("hello")
     public String hello(Model model){
         model.addAttribute("data","hello!!");
         System.out.println("hello world");
-        return "hello";
+        return "Hello World!";
     }
 
-    @RequestMapping(method = RequestMethod.GET, path = "hello/dto")
+    @RequestMapping(method = RequestMethod.GET, path = "/hello/dto")
     public BookDTO helloBook(@RequestParam("title") String title,
                              @RequestParam("price") int price,
                              @RequestParam("writer") String writer,
                              @RequestParam("publisher") String publisher) {
         //현재 RequestParam으로 모든 값을 일일히 가져오는데
         //RequestParam("DTO") BookDTO [변수명] 처럼 한번에 값을 받아올 수도 있다
-
+        System.out.println("hello");
         BookDTO book1 = BookDTO.builder()
                 .title(title)
                 .price(price)
